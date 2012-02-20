2b991bb9d751e3e6d66ec22755eb488713c7f6c3
Zach Mullen <zach.mullen@kitware.com>
2012-02-20 18:26:38 -0500

    Add breadcrumb bar for the extension category

--- public/js/extension/extension.view.js
+++ public/js/extension/extension.view.js
@@ -1,7 +1,28 @@
+var midas = midas || {};
+midas.slicerappstore = midas.slicerappstore || {};
+
+midas.slicerappstore.renderCategory = function(category) {
+    var currToken = '';
+    var categories = category.split('.');
+    $.each(categories, function(k, token) {
+        currToken += token;
+        var html = ' &gt; ';
+        html += '<a class="breadcrumbLink" href="'+json.global.webroot+'/slicerappstore?category='+currToken;
+        if(json.slicerView) {
+            html += '&slicerView';
+        }
+        html += '">';
+        html += token+'</a>';
+        currToken += '.';
+        $('#categoryBreadcrumb').append(html);
+    });
+}
+
 var json = null;
 
 $(document).ready(function() {
     json = $.parseJSON($('#jsonContent').html());
+    midas.slicerappstore.renderCategory(json.extension.category);
 
     $('#commentsDiv h4').remove();
     $('#ratingsDiv h4').remove();
