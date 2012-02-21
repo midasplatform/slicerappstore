4728e003d9de428a43005ad91a0598414f207017
Zach Mullen <zach.mullen@kitware.com>
2012-02-21 13:53:06 -0500

    Screenshot display for extensions (includes lightbox large view)

--- public/js/extension/extension.view.js
+++ public/js/extension/extension.view.js
@@ -1,6 +1,11 @@
 var midas = midas || {};
 midas.slicerappstore = midas.slicerappstore || {};
 
+var json = null;
+
+/**
+ * Renders the category of this extension as a breadcrumb bar
+ */
 midas.slicerappstore.renderCategory = function(category) {
     var currToken = '';
     var categories = category.split('.');
@@ -16,13 +21,43 @@ midas.slicerappstore.renderCategory = function(category) {
         currToken += '.';
         $('#categoryBreadcrumb').append(html);
     });
-}
+};
 
-var json = null;
+/**
+ * Renders the screenshots for this extension. Expects a space separated list of URLs
+ */
+midas.slicerappstore.renderScreenshots = function(screenshots) {
+  if(screenshots == '') {
+      $('div.screenshots').html('There are no screenshots for this extension.');
+      return;
+  }
+  screenshots = screenshots.split(' ');
+  $.each(screenshots, function(k, url) {
+      if(url != '') {
+          var template = $('#screenshotTemplate').clone();
+          template.removeAttr('id');
+          var link = template.find('a.screenshotPreview');
+          link.attr('href', url);
+          var img = link.find('img.screenshotPreview');
+          img.attr('src', url);
+          img.attr('alt', url);
+          template.appendTo('div.screenshots');
+          template.show();
+      }
+  });
+  $('div.screenshots a.screenshotPreview').lightBox({
+      imageLoading: json.global.moduleWebroot+'/public/images/lightbox/lightbox-ico-loading.gif',
+      imageBlank: json.global.moduleWebroot+'/public/images/lightbox/lightbox-blank.gif',
+      imageBtnClose: json.global.moduleWebroot+'/public/images/lightbox/lightbox-btn-close.gif',
+      imageBtnPrev: json.global.moduleWebroot+'/public/images/lightbox/lightbox-btn-prev.gif',
+      imageBtnNext: json.global.moduleWebroot+'/public/images/lightbox/lightbox-btn-next.gif'
+  });
+}
 
 $(document).ready(function() {
     json = $.parseJSON($('#jsonContent').html());
     midas.slicerappstore.renderCategory(json.extension.category);
+    midas.slicerappstore.renderScreenshots(json.extension.screenshots);
 
     if(json.slicerView) {
         createNotice = function() {}; //dummy function definition to prevent exceptions
